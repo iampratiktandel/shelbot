4a0fa1d4fa961839e2fdbdbd05e02acc2a8a9c6b
iampratiktandel <iampratiktandel@gmail.com>
2020-07-13 00:29:12 +0530

    Feat: Improved Bot Response and Added Scorecard to display scores

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
 const { prefix } = require('./config.json');
 const client = new Discord.Client();
+let bScore = 0, uScore = 0;
 
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -40,30 +41,94 @@ client.on('message', message => {
 
 		if (botReply === userReply) return message.reply('It\'s a tie!');
 
-		switch (userReply) {
-		case 'rock': {
-			if (botReply === 'paper' || botReply === 'spock') return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n Bot won!`);
-			else return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n You won!`);
+		if (userReply === 'rock') {
+			if (botReply === 'paper') {
+				message.reply('\n Bot covered your rock with paper\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'spock') {
+				message.reply('\n Bot\'s spock vaporised your rock\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'scissors') {
+				message.reply('\n You crushed Bot\'s scissors with rock\n You won!');
+				uScore += 1;
+			} else {
+				message.reply('\n You crushed Bot\'s lizard with rock\n You won!');
+				uScore += 1;
+			}
 		}
-		case 'paper': {
-			if (botReply === 'scissors' || botReply === 'lizard') return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n Bot won!`);
-			else return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n You won!`);
+		else if (userReply === 'paper') {
+			if (botReply === 'scissors') {
+				message.reply('\n Bot cut your paper with scissors\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'lizard') {
+				message.reply('\n Bot\'s lizard ate your paper\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'rock') {
+				message.reply('\n You covered Bot\'s rock with paper\n You won!');
+				uScore += 1;
+			} else {
+				message.reply('\n Your paper disproved Bot\'s spock\n You won!');
+				uScore += 1;
+			}
 		}
-		case 'scissors': {
-			if (botReply === 'rock' || botReply === 'spock') return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n Bot won!`);
-			else return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n You won!`);
+		else if (userReply === 'scissors') {
+			if (botReply === 'rock') {
+				message.reply('\n Bot crushed your scissors with rock\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'spock') {
+				message.reply('\n Bot\'s spock smashed your scissors\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'paper') {
+				message.reply('\n You cut Bot\'s paper with scissors\n You won!');
+				uScore += 1;
+			} else {
+				message.reply('\n You decapitated Bot\'s lizard with scissors\n You won!');
+				uScore += 1;
+			}
 		}
-		case 'lizard': {
-			if (botReply === 'rock' || botReply === 'scissors') return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n Bot won!`);
-			else return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n You won!`);
+		else if (userReply === 'lizard') {
+			if (botReply === 'rock') {
+				message.reply('\n Bot crushed your lizard with rock\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'scissors') {
+				message.reply('\n Bot decapitated your lizard with scissors\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'paper') {
+				message.reply('\n Your lizard ate Bot\'s paper\n You won!');
+				uScore += 1;
+			} else {
+				message.reply('\n Your lizard poisoned Bot\'s spock\n You won!');
+				uScore += 1;
+			}
 		}
-		case 'spock': {
-			if (botReply === 'lizard' || botReply === 'paper') return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n Bot won!`);
-			else return message.reply(`\n Your Choice : ${userReply}\n Bot's Choice : ${botReply}\n You won!`);
+		else if (userReply === 'spock') {
+			if (botReply === 'paper') {
+				message.reply('\n Bot\'s paper disproved your spock\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'lizard') {
+				message.reply('\n Bot\'s lizard poisoned your spock\n Bot won!');
+				bScore += 1;
+			} else if (botReply === 'rock') {
+				message.reply('\n Your spock vaporised Bot\'s rock\n You won!');
+				uScore += 1;
+			} else {
+				message.reply('\n Your spock smashed Bot\'s scissors\n You won!');
+				uScore += 1;
+			}
 		}
-		default: {
-			return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
+		else {
+			message.reply(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
 		}
+
+		message.channel.send(`------SCORECARD------\n  Bot: ${bScore}\n  User: ${uScore}\n---------------------------`);
+		if (bScore == 5) {
+			message.reply('Bot won the match');
+			bScore = 0;
+			uScore = 0;
+		} else if (uScore == 5) {
+			message.reply('You won the match');
+			bScore = 0;
+			uScore = 0;
 		}
 	}
 });
